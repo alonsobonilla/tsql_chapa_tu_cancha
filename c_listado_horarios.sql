7e9b6dd1123e1ec9d9f6950da992b952c47ae479
Alonso Bonilla <luis.labp06@gmail.com>
2023-10-14 18:54:27 -0500

    Hour list query changed

--- c_listado_horarios.sql
+++ c_listado_horarios.sql
@@ -1,12 +1,14 @@
 SELECT c2.nombre_cancha, consulta.*
 FROM (SELECT h.id_cancha,
-             dia_semana,
-             dbo.fn_hora_rango(h.id_cancha, dia_semana, 1)
+             datename(weekday, fecha) as dia,
+             fecha,
+             dbo.fn_hora_rango(h.id_cancha, fecha, 1)
                  AS hora_inicio,
-             dbo.fn_hora_rango(h.id_cancha, dia_semana, 0)
+             dbo.fn_hora_rango(h.id_cancha, fecha, 0)
                  AS hora_fin
       FROM horario h
                INNER JOIN cancha c ON h.id_cancha = c.id_cancha
       WHERE c.id_complejo = 1
-      GROUP BY h.id_cancha, dia_semana) AS consulta
+        and fecha >= cast(getdate() as date)
+      GROUP BY h.id_cancha, fecha) AS consulta
          INNER JOIN cancha c2 ON c2.id_cancha = consulta.id_cancha
